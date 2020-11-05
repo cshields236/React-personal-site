ad553b6f8f3ffa5fc4cf967339a65c1a008a43b6
Conor Shields <conor.shields1@mydit.ie>
2020-11-05 21:49:40 +0000

    showing contact screen - no functionality

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero';
 import Content from '../components/Content'
+import { Button, Form } from 'react-bootstrap';
 
 class ContactPage extends React.Component {
     constructor(props) {
@@ -9,7 +10,7 @@ class ContactPage extends React.Component {
             name: '',
             email: '',
             message: '',
-            disables: false,
+            disabled: false,
             emailSent: null
         }
     }
@@ -20,7 +21,29 @@ class ContactPage extends React.Component {
                 <Hero title={this.props.title}/>
 
                 <Content>
-                    Hey Yo
+                    <Form onSubmit={this.handleSubmit}> 
+                        <Form.Group>
+                            <Form.Label htmlFor='full-name'>Full Name</Form.Label>
+                            <Form.Control id='full-name' name='name' type='text' value={this.state.name} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label htmlFor='email'>Email</Form.Label>
+                            <Form.Control id='email' name='email' type='email' value={this.state.email} onChange={this.handleChange} />
+                        </Form.Group>
+                        
+                        <Form.Group>
+                            <Form.Label htmlFor='message'>Message</Form.Label>
+                            <Form.Control id='message' name='message' as='textarea' rows='3' value={this.state.message} onChange={this.handleChange} />
+                        </Form.Group>
+
+                        <Button className='d-inline-block' variant='primary' type='submit' disabled={this.state.disabled}>
+                            Send
+                        </Button>
+
+                        {this.state.emailSent === true && <p className='d-inline success-msg'>Email Sent</p>}
+                        {this.state.emailSent === false && <p className='d-inline error-msg'>Email Not Sent</p>}
+                    </Form>
                 </Content>
             </div>
         )

0ffd92780bdee48718433bf86f9328498b5acd83
Conor Shields <conor.shields1@mydit.ie>
2020-11-05 15:20:24 +0000

    adding top navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Container } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav'
 
 class App extends React.Component {
 
@@ -33,8 +34,18 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-        <Container fluid={true}>
-          <p>Hello</p>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg='transparent' expand='lg'>
+            <Navbar.Brand>Conor Shields</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-toggle" />
+            <Navbar.Collapse id='nav-toggle'>
+              <Nav className='ml-auto'>
+                <Link className='nav-link' to=''>Home</Link>
+                <Link className='nav-link' to=''>About</Link>
+                <Link className='nav-link' to=''>Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         </Container>
       </Router>
     )

32f64fe74349130281716f93d8a93d668f0bd58a
Conor Shields <conor.shields1@mydit.ie>
2020-11-05 15:02:21 +0000

    adding content to state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,27 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state {
-      
+    this.state = {
+        title: 'Conor Shields', 
+        headerLinks: [
+          { title: 'Home', path: '/'},
+          { title: 'About', path: '/about'},
+          { title: 'Contact', path: '/contact'}
+        
+        ],
+      home:{
+        title: 'Conor Shields',
+        subTitle: 'Projects of Mine',
+        text: 'Have a look at my projects'
+      },
+      about:{
+        title: 'About Me',
+        
+      },
+      contact:{
+        title: 'Contact Me',
+        
+      },
     }
   }
   render(){

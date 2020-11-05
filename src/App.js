a93054c963451338ae5e489ebd267754fff2b5b5
Conor Shields <conor.shields1@mydit.ie>
2020-11-05 15:06:25 +0000

    adding router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React from  'react'
+import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        title: 'Conor Shields', 
-        headerLinks: [
-          { title: 'Home', path: '/'},
-          { title: 'About', path: '/about'},
-          { title: 'Contact', path: '/contact'}
-        
-        ],
-      home:{
+      title: 'Conor Shields',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
+
+      ],
+      home: {
         title: 'Conor Shields',
         subTitle: 'Projects of Mine',
         text: 'Have a look at my projects'
       },
-      about:{
+      about: {
         title: 'About Me',
-        
+
       },
-      contact:{
+      contact: {
         title: 'Contact Me',
-        
+
       },
     }
   }
-  render(){
+  render() {
     return (
-      <div>
-        Hello 
-      </div>
-    )}
+      <Router>
+
+      </Router>
+    )
+  }
 }
 
 export default App;
